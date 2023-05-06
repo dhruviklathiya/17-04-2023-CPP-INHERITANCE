fe1a22577751e426895fe4ed4a05b97ed0ffa6be
DHRUVIK LATHIYA <115994798+dhruviklathiya@users.noreply.github.com>
2023-05-06 12:39:43 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,18 +8,14 @@ class X{
 };
 class Y : public X{
     public:
-    void inputData(){
-        cout<<"Enter value of a:";
-        cin>>this->a;
-        cout<<"Enter value of b:";
-        cin>>this->b;
-        cout<<"Enter value of c:";
-        cin>>this->c;
-    }
     void setData(){
-        this->a = a;
-        this->b = b;
-        this->c = c;
+        cout<<"Please enter parameter in a,b,c for getting sum of all three number's cube :"<<endl;
+        cout<<"Enter value of a:";
+        cin>>a;
+        cout<<"Enter value of b:";
+        cin>>b;
+        cout<<"Enter value of c:";
+        cin>>c;
     }
     void getData(){
         int d = a+b+c;
@@ -32,8 +28,49 @@ class Y : public X{
 };
 int main () {
     Y obj1;
-    obj1.inputData();
     obj1.setData();
     obj1.getData();
     return 0;
 }
+
+
+// WAY 2:
+// #include<iostream>
+// using namespace std;
+// class X{
+//     public:
+//     int a;
+//     int b;
+//     int c;
+// };
+// class Y : public X{
+//     public:
+//     void inputData(){
+//         cout<<"Enter value of a:";
+//         cin>>this->a;
+//         cout<<"Enter value of b:";
+//         cin>>this->b;
+//         cout<<"Enter value of c:";
+//         cin>>this->c;
+//     }
+//     void setData(){
+//         this->a = a;
+//         this->b = b;
+//         this->c = c;
+//     }
+//     void getData(){
+//         int d = a + b + c;
+//         int e = d * d * d;
+//         int f = (a*a*a) + (b*b*b) + (c*c*c);
+//         cout<<"Sum of all three number is: "<<d<<endl;
+//         cout<<"Sum of all three number’s cubes is: "<<e<<endl;
+//         cout<<"All three numbers cube's sum is: "<<f<<endl;
+//     }
+// };
+// int main () {
+//     Y obj1;
+//     obj1.inputData();
+//     obj1.setData();
+//     obj1.getData();
+//     return 0;
+// }
